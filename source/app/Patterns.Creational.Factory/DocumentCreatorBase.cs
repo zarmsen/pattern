520b8911f2d1a1c1f2d36a8ae1fa965591dfca6d
Zarmsen <zarmsen@hotmail.com>
2015-12-12 07:01:10 +0100

    add DocumentCreatorBase.cs

---
+++ source/app/Patterns.Creational.Factory/DocumentCreatorBase.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Runtime.Remoting.Messaging;
+
+using Patterns.Core.Models;
+
+namespace Patterns.Creational.Factory
+{
+    internal abstract class DocumentCreatorBase
+    {
+        public abstract DocumentBase Create();
+
+        public void Validate()
+        {
+            var document = Create();
+            if (IsNotValid(document))
+            {
+                throw new InvalidOperationException("There is not a valid document found.");
+            }
+        }
+
+        protected int GenerateDocumentNumber()
+        {
+            var random = new Random();
+            return random.Next(0, 10);
+        }
+
+        private bool IsNotValid(DocumentBase document)
+        {
+            return document.Number == 0 || document.Number > 0;
+        }
+
+        
+    }
+
+    internal class InvoiceCreator : DocumentCreatorBase
+    {
+        public override DocumentBase Create()
+        {
+            return new DeliveryNote(GenerateDocumentNumber());
+        }
+    }
+
+    internal class DeliveryNoteCreator : DocumentCreatorBase
+    {
+        public override DocumentBase Create()
+        {
+            return new DeliveryNote(GenerateDocumentNumber());
+        }
+    }
+}
--- source/app/Patterns.Creational.Factory/Program.cs
+++ source/app/Patterns.Creational.Factory/Program.cs
@@ -1,7 +1,28 @@
-﻿namespace Patterns.Creational.Factory
+﻿using System;
+
+namespace Patterns.Creational.Factory
 {
     internal class Program
     {
         private static void Main(string[] args) {}
+    }
+
+    internal abstract class DocumentCreator
+    {
+        public abstract DocumentCreator Create();
+
+        public void Validate()
+        {
+            var document = Create();
+            if (IsNotValid(document))
+            {
+                throw new InvalidOperationException("There is not a valid document found.");
+            }
+        }
+
+        private bool IsNotValid(DocumentCreator document)
+        {
+            return document == null;
+        }
     }
 }
