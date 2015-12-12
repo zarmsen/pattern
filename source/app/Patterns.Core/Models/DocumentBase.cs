be78d821b566196365451b69725fa51115787420
Zarmsen <zarmsen@hotmail.com>
2015-12-12 06:33:51 +0100

    add DocumentBase.cs

---
+++ source/app/Patterns.Core/Models/DocumentBase.cs
@@ -0,0 +1,12 @@
+﻿namespace Patterns.Core.Models
+{
+    public abstract class DocumentBase
+    {
+        public string Name { get; private set; }
+
+        public DocumentBase(string name)
+        {
+            Name = name;
+        }
+    }
+}
