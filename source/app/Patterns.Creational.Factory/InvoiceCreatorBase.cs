79c85fad4837e866a8635e3fb90e96b9b3981f86
Zarmsen <zarmsen@hotmail.com>
2015-12-13 06:41:16 +0100

    create InvoiceCreatorBase.cs

---
+++ source/app/Patterns.Creational.Factory/InvoiceCreatorBase.cs
@@ -0,0 +1,32 @@
+﻿using Patterns.Core.Models;
+
+namespace Patterns.Creational.Factory
+{
+    internal abstract class InvoiceCreatorBase : DocumentCreatorBase
+    {
+        private readonly InvoiceType _typeOfInvoice;
+
+        protected InvoiceCreatorBase(InvoiceType typeOfInvoice)
+        {
+            _typeOfInvoice = typeOfInvoice;
+        }
+
+        public override DocumentBase Create()
+        {
+            switch (_typeOfInvoice)
+            {
+                case InvoiceType.PartialPayment:
+                {
+                    return new PartialPaymentInvoice(GenerateDocumentNumber());
+                }
+                case InvoiceType.Performa:
+                {
+                    return new PerformaInvoice(GenerateDocumentNumber());
+                }
+
+                default:
+                    return null;
+            }
+        }
+    }
+}
