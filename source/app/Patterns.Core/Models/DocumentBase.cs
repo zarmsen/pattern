eeb06d5587450b12ef54fe21ab7d0e867f7d98d4
Zarmsen <zarmsen@hotmail.com>
2015-12-13 06:48:16 +0100

    override ToString

--- source/app/Patterns.Core/Models/DocumentBase.cs
+++ source/app/Patterns.Core/Models/DocumentBase.cs
@@ -2,19 +2,29 @@
 {
     public abstract class DocumentBase
     {
-        public DocumentBase(int number)
+        protected DocumentBase(int number)
         {
             Number = number;
         }
+
+        public int Number { get; }
+
+        public string Message { get { return string.Format("Found document '{0}' with number {1}", ToString(), Number); } }
 
         public virtual bool IsValid()
         {
             return Number > 0;
         }
 
-
-        public int Number { get; }
-
-        public string Message { get { return string.Format("Found document '{0}' with number {1}", ToString(), Number); } }
+        /// <summary>
+        ///     Returns a string that represents the current object.
+        /// </summary>
+        /// <returns>
+        ///     A string that represents the current object.
+        /// </returns>
+        public override string ToString()
+        {
+            return GetType().Name;
+        }
     }
 }
