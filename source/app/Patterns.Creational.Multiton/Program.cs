fa3a9fda5c3deadb7bb0f0e18d1ba8302159931c
Zarmsen <zarmsen@hotmail.com>
2015-12-19 07:08:41 +0100

    implement DbConnectionPool example

--- source/app/Patterns.Creational.Multiton/Program.cs
+++ source/app/Patterns.Creational.Multiton/Program.cs
@@ -1,7 +1,21 @@
-﻿namespace Patterns.Creational.Multiton
+﻿using System;
+
+namespace Patterns.Creational.Multiton
 {
     internal class Program
     {
-        private static void Main(string[] args) {}
+        private static void Main(string[] args)
+        {
+            DbConnectionPool.Instance[2].Open();
+            DbConnectionPool.Instance[5].Open();
+            DbConnectionPool.Instance[8].Open();
+
+            Console.WriteLine("Press Enter to open all instances again.");
+            Console.ReadKey();
+
+            DbConnectionPool.Instance.OpenAll();
+
+            Console.ReadKey();
+        }
     }
 }
