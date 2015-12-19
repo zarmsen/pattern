79797aaac0e13826f211e9b5b6f0bb5ea5c65eac
Zarmsen <zarmsen@hotmail.com>
2015-12-19 06:40:11 +0100

    create DbConnection

---
+++ source/app/Patterns.Core/DbConnection.cs
@@ -0,0 +1,90 @@
+﻿using System;
+
+namespace Patterns.Core
+{
+    public class DbConnection : IEquatable<DbConnection>
+    {
+        public DbConnection(string connection)
+        {
+            Connection = connection;
+        }
+
+        public string Connection { get; }
+
+        /// <summary>
+        ///     Indicates whether the current object is equal to another object of the same type.
+        /// </summary>
+        /// <returns>
+        ///     true if the current object is equal to the <paramref name="other"/> parameter; otherwise, false.
+        /// </returns>
+        /// <param name="other">An object to compare with this object.</param>
+        public bool Equals(DbConnection other)
+        {
+            if (ReferenceEquals(null, other))
+            {
+                return false;
+            }
+            if (ReferenceEquals(this, other))
+            {
+                return true;
+            }
+            return string.Equals(Connection, other.Connection);
+        }
+
+        /// <summary>
+        ///     Returns a string that represents the current object.
+        /// </summary>
+        /// <returns>
+        ///     A string that represents the current object.
+        /// </returns>
+        public override string ToString()
+        {
+            return $"Connection: {Connection} ({GetHashCode()})";
+        }
+
+        /// <summary>
+        ///     Determines whether the specified object is equal to the current object.
+        /// </summary>
+        /// <returns>
+        ///     true if the specified object  is equal to the current object; otherwise, false.
+        /// </returns>
+        /// <param name="obj">The object to compare with the current object. </param>
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj))
+            {
+                return false;
+            }
+            if (ReferenceEquals(this, obj))
+            {
+                return true;
+            }
+            if (obj.GetType() != GetType())
+            {
+                return false;
+            }
+            return Equals((DbConnection)obj);
+        }
+
+        /// <summary>
+        ///     Serves as the default hash function.
+        /// </summary>
+        /// <returns>
+        ///     A hash code for the current object.
+        /// </returns>
+        public override int GetHashCode()
+        {
+            return (Connection != null ? Connection.GetHashCode() : 0);
+        }
+
+        public static bool operator ==(DbConnection left, DbConnection right)
+        {
+            return Equals(left, right);
+        }
+
+        public static bool operator !=(DbConnection left, DbConnection right)
+        {
+            return !Equals(left, right);
+        }
+    }
+}
