51196297c90a7dd88297c1aca87be5dd9bad5206
Zarmsen <zarmsen@hotmail.com>
2015-12-20 15:04:13 +0100

    create example

--- source/app/Patterns.Creational.AbstractFactory/Program.cs
+++ source/app/Patterns.Creational.AbstractFactory/Program.cs
@@ -1,7 +1,19 @@
-﻿namespace Patterns.Creational.AbstractFactory
+﻿using System;
+
+namespace Patterns.Creational.AbstractFactory
 {
     internal class Program
     {
-        private static void Main(string[] args) {}
+        private static void Main(string[] args)
+        {
+            
+            var gameController = new GameController(new CounterStrikeFactory());
+
+            gameController.Run();
+       
+
+            Console.ReadKey();
+
+        }
     }
 }
