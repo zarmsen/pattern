31b4c88ac781bbae330020b774a9702df5599239
Zarmsen <zarmsen@hotmail.com>
2015-12-20 13:49:36 +0100

    create playerbase

---
+++ source/app/Patterns.Core/Game/PlayerBase.cs
@@ -0,0 +1,15 @@
+﻿namespace Patterns.Core.Game
+{
+    public class PlayerBase
+    {
+        public string Name { get; private set; }
+
+        public PlayerBase(string name)
+        {
+            Energy = 100;
+            Name = name;
+        }
+
+        public int Energy { get; set; }
+    }
+}
