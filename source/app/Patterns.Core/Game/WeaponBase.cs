5a2ea4fb75fa7188a01b2fc4e803099b45d281dd
Zarmsen <zarmsen@hotmail.com>
2015-12-20 14:02:20 +0100

    generate equality members

--- source/app/Patterns.Core/Game/WeaponBase.cs
+++ source/app/Patterns.Core/Game/WeaponBase.cs
@@ -1,6 +1,8 @@
-﻿namespace Patterns.Core.Game
+﻿using System;
+
+namespace Patterns.Core.Game
 {
-    public abstract class WeaponBase
+    public abstract class WeaponBase : IEquatable<WeaponBase>
     {
         public string Name { get;  private set; }
 
@@ -10,6 +12,71 @@
         {
             Name = name;
             Effect = effect;
+        }
+
+        /// <summary>
+        /// Indicates whether the current object is equal to another object of the same type.
+        /// </summary>
+        /// <returns>
+        /// true if the current object is equal to the <paramref name="other"/> parameter; otherwise, false.
+        /// </returns>
+        /// <param name="other">An object to compare with this object.</param>
+        public bool Equals(WeaponBase other)
+        {
+            if (ReferenceEquals(null, other))
+            {
+                return false;
+            }
+            if (ReferenceEquals(this, other))
+            {
+                return true;
+            }
+            return string.Equals(Name, other.Name);
+        }
+
+        /// <summary>
+        /// Determines whether the specified object is equal to the current object.
+        /// </summary>
+        /// <returns>
+        /// true if the specified object  is equal to the current object; otherwise, false.
+        /// </returns>
+        /// <param name="obj">The object to compare with the current object. </param>
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj))
+            {
+                return false;
+            }
+            if (ReferenceEquals(this, obj))
+            {
+                return true;
+            }
+            if (obj.GetType() != this.GetType())
+            {
+                return false;
+            }
+            return Equals((WeaponBase)obj);
+        }
+
+        /// <summary>
+        /// Serves as the default hash function. 
+        /// </summary>
+        /// <returns>
+        /// A hash code for the current object.
+        /// </returns>
+        public override int GetHashCode()
+        {
+            return (Name != null ? Name.GetHashCode() : 0);
+        }
+
+        public static bool operator ==(WeaponBase left, WeaponBase right)
+        {
+            return Equals(left, right);
+        }
+
+        public static bool operator !=(WeaponBase left, WeaponBase right)
+        {
+            return !Equals(left, right);
         }
     }
 }
