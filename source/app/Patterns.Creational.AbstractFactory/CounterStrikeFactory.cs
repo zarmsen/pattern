5e94600637f88cea2bbd3b17c09fa8f059488a26
Zarmsen <zarmsen@hotmail.com>
2015-12-20 14:35:40 +0100

    create IGameFactory

---
+++ source/app/Patterns.Creational.AbstractFactory/CounterStrikeFactory.cs
@@ -0,0 +1,17 @@
+﻿using Patterns.Core.Game;
+
+namespace Patterns.Creational.AbstractFactory
+{
+    public class CounterStrikeFactory : IGameFactory
+    {
+        public WeaponBase CreateWeapon()
+        {
+            return null;
+        }
+
+        public PlayerBase CreatePlayer()
+        {
+            return null;
+        }
+    }
+}
---
+++ source/app/Patterns.Creational.AbstractFactory/HeloFactory.cs
@@ -0,0 +1,17 @@
+﻿using Patterns.Core.Game;
+
+namespace Patterns.Creational.AbstractFactory
+{
+    public class HeloFactory : IGameFactory
+    {
+        public WeaponBase CreateWeapon()
+        {
+            return null;
+        }
+
+        public PlayerBase CreatePlayer()
+        {
+            return null;
+        }
+    }
+}
