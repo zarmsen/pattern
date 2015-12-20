b30d9e4f476cdaf92c310246073c514d24e73285
Zarmsen <zarmsen@hotmail.com>
2015-12-20 14:15:48 +0100

    crreate weapons

---
+++ source/app/Patterns.Core/Game/Gun.cs
@@ -0,0 +1,11 @@
+﻿namespace Patterns.Core.Game
+{
+    public class Gun : WeaponBase
+    {
+        public Gun()
+            : this(50) {}
+
+        public Gun(int effect)
+            : base("Gun", effect) {}
+    }
+}
