cab8d15acc4a6d4ac3b6bf32b58604226d76a95a
Zarmsen <zarmsen@hotmail.com>
2015-12-20 15:04:01 +0100

    create GameController

--- source/app/Patterns.Creational.AbstractFactory/GameController.cs
+++ source/app/Patterns.Creational.AbstractFactory/GameController.cs
@@ -16,37 +16,45 @@ namespace Patterns.Creational.AbstractFactory
 
         public GameController(IGameFactory game)
         {
-            _random = new Random(2);
+            _random = new Random(10);
             _fighter = game.CreateFighter();
             _victim = game.CreateVictim();
             _weapon = game.CreateWeapon();
         }
 
-        public void Move()
+        public void Run()
         {
-            if (_fighter.Energy == 0)
-            {
-                Console.WriteLine("The fighter {0} is death.", _fighter);
-                return;
-            }
 
-            if (_victim.Energy == 0)
+            while (_fighter.Energy != 0 || _victim.Energy != 0)
             {
-                Console.WriteLine("The fighter {0} is death.", _victim);
-                return;
-            }
 
-            var isFighterHit = _random.Next(1, 2) == 1;
 
-            if (isFighterHit)
-            {
-                var hitResult = _weapon.Hit(_victim);
-                _victim.GetHit(hitResult);
-            }
-            else
-            {
-                var hitResult = _weapon.Hit(_fighter);
-                _fighter.GetHit(hitResult);
+                if (_fighter.Energy == 0)
+                {
+                    Console.WriteLine("The fighter {0} is death.", _fighter);
+                    return;
+                }
+
+                if (_victim.Energy == 0)
+                {
+                    Console.WriteLine("The fighter {0} is death.", _victim);
+                    return;
+                }
+
+                var isFighterHit = _random.Next(1, 2) == 1;
+
+                if (isFighterHit)
+                {
+                    var hitResult = _weapon.Hit(_victim);
+                    _victim.GetHit(hitResult);
+                }
+                else
+                {
+                    var hitResult = _weapon.Hit(_fighter);
+                    _fighter.GetHit(hitResult);
+                }
+
+                Console.WriteLine("The energy of fighter is {0} and the energy of victim is {1}.", _fighter.Energy, _victim.Energy);
             }
         }
     }
