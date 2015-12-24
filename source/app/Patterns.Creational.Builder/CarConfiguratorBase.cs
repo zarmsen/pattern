368327395eb813a17a5dd89aec17db3fc3347495
Zarmsen <zarmsen@hotmail.com>
2015-12-24 06:48:38 +0100

    create class CarConfiguratorBase

---
+++ source/app/Patterns.Creational.Builder/CarConfiguratorBase.cs
@@ -0,0 +1,18 @@
+﻿using Patterns.Core.Vehicle;
+
+namespace Patterns.Creational.Builder
+{
+    internal abstract class CarConfiguratorBase
+    {
+        private Car _car;
+
+        protected CarConfiguratorBase(string manufacturerName)
+        {
+            _car = new Car(manufacturerName);
+        }
+
+        public abstract void Build();
+
+        public abstract void AddEquipment(Equipment equipment);
+    }
+}
