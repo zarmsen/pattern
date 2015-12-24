7d94348cecb79b44ea884a91b1f38fad3783f55e
Zarmsen <zarmsen@hotmail.com>
2015-12-24 06:11:17 +0100

    create project builder

---
+++ source/app/Patterns.Creational.Builder/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Patterns.Creational.Builder
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.ReadKey();
+        }
+    }
+}
