4d4ca720926f02ba4364ec3a6bac4d6d5bb7c8e5
Zarmsen <zarmsen@hotmail.com>
2015-12-24 08:11:19 +0100

    create prototype example

--- source/app/Patterns.Creational.Prototype/Program.cs
+++ source/app/Patterns.Creational.Prototype/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+
+using Patterns.Core.Vehicle;
 
 namespace Patterns.Creational.Prototype
 {
@@ -6,6 +9,23 @@ namespace Patterns.Creational.Prototype
     {
         private static void Main(string[] args)
         {
+
+
+            var car = new CarProtoType(new Car("Vw"));
+            car.Add(new Equipment("Engine"));
+            
+            var copyOfCar = car.DeepCopy();
+           
+            copyOfCar.Add(new Equipment("Engine 2"));
+
+
+            Console.WriteLine(copyOfCar.Car.Manufacturer);
+
+            foreach (var variable in copyOfCar.Car.Equipments)
+            {
+                Console.WriteLine(variable.Name);
+            }
+
             Console.ReadKey();
         }
     }
