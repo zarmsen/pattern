2b9dc0847f9385bba2ac894f580e46047a6e6baa
Zarmsen <zarmsen@hotmail.com>
2015-12-25 05:29:47 +0100

    create project structural  adapter

---
+++ source/app/Patterns.Structural.Adapter/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Patterns.Structural.Adapter
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.ReadKey();
+        }
+    }
+}
