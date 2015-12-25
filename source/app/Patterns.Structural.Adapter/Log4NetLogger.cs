9bb70303cbc1fd421c948842cdb50297c3b1ea7c
Zarmsen <zarmsen@hotmail.com>
2015-12-25 07:14:43 +0100

    create class Log4NetLogger

---
+++ source/app/Patterns.Structural.Adapter/Log4NetLogger.cs
@@ -0,0 +1,18 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright © IcamSystems GmbH, 2015
+// Erstellt am: 25.12.2015 07:12
+// Autor: André Harms
+// --------------------------------------------------------------------------------
+
+using System;
+
+namespace Patterns.Structural.Adapter
+{
+    public class Log4NetLogger
+    {
+        public void Log(string type, string messsag)
+        {
+            Console.WriteLine("type:{0} message: {1}", type, messsag);
+        } 
+    }
+}
