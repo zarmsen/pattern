406c6ab679a4782698f49748ae199caf7a344186
Zarmsen <zarmsen@hotmail.com>
2015-12-26 06:55:15 +0100

    create project composite

---
+++ source/app/Patterns.Structural.Composite/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Patterns.Structural.Composite
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.ReadKey();
+        }
+    }
+}
