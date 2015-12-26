c957ef1939b8051c63bc823bf3525ff3888dad84
Zarmsen <zarmsen@hotmail.com>
2015-12-26 07:59:11 +0100

    create example

--- source/app/Patterns.Structural.Composite/Program.cs
+++ source/app/Patterns.Structural.Composite/Program.cs
@@ -6,7 +6,26 @@ namespace Patterns.Structural.Composite
     {
         private static void Main(string[] args)
         {
+            var file = new TreeNode(1, "File");
+            file.Add(new TreeNode(2, "Save"));
+            file.Add(new TreeNode(3, "End"));
+            var edit = new TreeNode(4, "Edit") { new TreeNode(5, "Paste"), new TreeNode(6, "Copy") };
+            file.Add(edit);
+
+            Output(file);
+            Output(edit);
+
             Console.ReadKey();
+        }
+
+        private static void Output(TreeNode nodes)
+        {
+            nodes.Output();
+
+            foreach (var node in nodes)
+            {
+               Console.WriteLine("\t{0}", node);
+            }
         }
     }
 }
