34af845bdd1ee57e82d323b506e8967526b5bf0e
Zarmsen <zarmsen@hotmail.com>
2016-01-10 05:37:16 +0100

    create subsystems

---
+++ source/app/Patterns.Structural.Facade/SubSystemOne.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Patterns.Structural.Facade
+{
+    internal class SubSystemOne
+    {
+        public void ReadOne()
+        {
+            Console.WriteLine("Read One!");
+        }
+    }
+}
