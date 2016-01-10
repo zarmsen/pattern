691674ff90b9d89cb230570077ac21bd62d73629
Zarmsen <zarmsen@hotmail.com>
2016-01-10 06:18:19 +0100

    create project structural flyweight

---
+++ source/app/Patterns.Structural.Flyweight/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Patterns.Structural.Flyweight
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.ReadKey();
+        }
+    }
+}
