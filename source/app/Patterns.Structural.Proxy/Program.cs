58c28d5dcbdb44f28d61eb2c91c03b9dd359848f
Zarmsen <zarmsen@hotmail.com>
2016-01-10 07:48:44 +0100

    create example for dynamic proxy

--- source/app/Patterns.Structural.Proxy/Program.cs
+++ source/app/Patterns.Structural.Proxy/Program.cs
@@ -7,8 +7,15 @@ namespace Patterns.Structural.Proxy
         private static void Main(string[] args)
         {
             var proxy = new MailLogProxy();
-
             proxy.Send();
+
+            var dynamicProxy = new LogProxy<IMail>(new Mail());
+            var mail = dynamicProxy.GetTransparentProxy() as IMail;
+
+            if (mail != null)
+            {
+                mail.Send();
+            }
 
             Console.ReadKey();
         }
