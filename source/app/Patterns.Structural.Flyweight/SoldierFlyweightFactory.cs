3ebda8fe5c29cdd057ea5e0f02077e344787888a
Zarmsen <zarmsen@hotmail.com>
2016-01-10 06:36:44 +0100

    create class SoldierFlyweightFactory

---
+++ source/app/Patterns.Structural.Flyweight/SoldierFlyweightFactory.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+using Patterns.Core.Game;
+
+namespace Patterns.Structural.Flyweight
+{
+    internal class SoldierFlyweightFactory
+    {
+
+        private Dictionary<Weapons, Soldier> _cache = new Dictionary<Weapons, Soldier>();
+
+        public int CountOfObjects => _cache.Count;
+
+        public Soldier GetSoldier(Weapons weapon)
+        {
+            var isAvailable = _cache.ContainsKey(weapon);
+            if (isAvailable)
+            {
+                return _cache[weapon];
+            }
+
+            var soldier = new Soldier("Soldier");
+            soldier.SetWeapon(weapon);
+            _cache.Add(weapon, soldier);
+            return soldier;
+        }
+    }
+}
