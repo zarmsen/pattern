7e465c70b94a2ad683328b7cec76049993512954
Zarmsen <zarmsen@hotmail.com>
2016-01-10 04:49:50 +0100

    create class PlayerDecoratorAddOn

---
+++ source/app/Patterns.Structural.Decorator/PlayerDecoratorAddOn.cs
@@ -0,0 +1,20 @@
+﻿using Patterns.Core.Game;
+
+namespace Patterns.Structural.Decorator
+{
+    internal abstract class PlayerDecoratorAddOn : PlayerBase
+    {
+        private readonly PlayerBase _player;
+
+        protected PlayerDecoratorAddOn(string name, PlayerBase player)
+            : base(name)
+        {
+            _player = player;
+        }
+
+        public sealed override void GetHit(HitResult result)
+        {
+            _player.GetHit(result);
+        }
+    }
+}
