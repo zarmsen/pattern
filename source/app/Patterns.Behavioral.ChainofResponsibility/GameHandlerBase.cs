94490719b309c51d66d280afd74ca6fd9a4e03c7
Zarmsen <zarmsen@hotmail.com>
2016-01-22 06:32:57 +0100

    create class GameHandlerBase

---
+++ source/app/Patterns.Behavioral.ChainofResponsibility/GameHandlerBase.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Patterns.Behavioral.ChainofResponsibility
+{
+    internal abstract class GameHandlerBase
+
+    {
+        private readonly IEnumerable<string> _commandList;
+
+        protected GameHandlerBase Successor;
+
+        protected GameHandlerBase(IEnumerable<string> commandList)
+        {
+            _commandList = commandList;
+        }
+
+        public void SetSuccessor(GameHandlerBase successor)
+        {
+            Successor = successor;
+        }
+
+        public void Execute(string command)
+        {
+            if (_commandList.Any(x => x.Contains(command)))
+            {
+                HandleRequest(command);
+            }
+            else
+            {
+                Successor.Execute(command);
+            }
+        }
+
+        protected abstract void HandleRequest(string command);
+    }
+}
