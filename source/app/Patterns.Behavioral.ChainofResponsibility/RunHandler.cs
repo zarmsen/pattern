e7f2ebaea5ff7cb889b386e17460144c35cb50ed
Zarmsen <zarmsen@hotmail.com>
2016-01-22 06:37:32 +0100

    create class RunHandler

---
+++ source/app/Patterns.Behavioral.ChainofResponsibility/RunHandler.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Patterns.Behavioral.ChainofResponsibility
+{
+    internal class RunHandler : GameHandlerBase
+    {
+        public RunHandler(): base(new List<string> {"run", "go"}) {}
+
+        protected override void HandleRequest(string command)
+        {
+            Console.WriteLine("Player is running, because he is pushing command '{0}'.", command);
+        }
+    }
+}
