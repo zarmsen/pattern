e7129c43495f0512d0979e7129ad65512aa657e2
Zarmsen <zarmsen@hotmail.com>
2016-01-23 06:40:10 +0100

    create project behavioral command

---
+++ source/app/Patterns.Behavioral.Command/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Patterns.Behavioral.Command
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.ReadKey();
+        }
+    }
+}
