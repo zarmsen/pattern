21c71631b1b892e14933f7e8358c125bff7ebe5a
Zarmsen <zarmsen@hotmail.com>
2016-01-23 07:10:05 +0100

    create class MoveConcreteCommand

---
+++ source/app/Patterns.Behavioral.Command/MoveConcreteCommand.cs
@@ -0,0 +1,35 @@
+using System;
+
+using Patterns.Core.Game;
+
+namespace Patterns.Behavioral.Command
+{
+    internal class MoveConcreteCommand : GameCommandBase
+    {
+        private readonly Position _position;
+
+        private Position _oldPosition;
+
+        public MoveConcreteCommand(PlayerBase player, Position position)
+            : base(player)
+        {
+            _position = position;
+        }
+
+        public override void Execute()
+        {
+            _oldPosition = Player.CurrentPosition;
+            Player.Move(_position);
+            Console.WriteLine("MOVE: Player {1} from position '{2}' to position '{0}'", _position, Player.Name, _oldPosition);
+        }
+
+        public override void Undo()
+        {
+            if (_oldPosition != null)
+            {
+                Player.Move(_oldPosition);
+                Console.WriteLine("UNDO: player {1} to position '{0}'", _position, Player.Name);
+            }
+        }
+    }
+}
