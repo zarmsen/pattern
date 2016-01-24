55d23471c7a2e2c310acf9c11fae7d5ad4145c1f
Zarmsen <zarmsen@hotmail.com>
2016-01-24 07:10:28 +0100

    create class LoggingInterceptor

---
+++ source/app/Patterns.Behavioral.Interceptor/LoggingInterceptor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Patterns.Behavioral.Interceptor
+{
+    internal class LoggingInterceptor : IInterceptor<GameContext>
+    {
+        public void HandleEvent(GameContext context, string search)
+        {
+            Console.WriteLine("User search to '{0}'!", search);
+            var player = context.GetPlayer(search);
+            if (player == null)
+            {
+                Console.WriteLine("Nobody in the framework!'{0}'!", search);
+            }
+            else
+            {
+                Console.WriteLine("Framework found player '{0}'.", player.Name);
+            }
+        }
+    }
+}
