d682c423a68f99db8c028e4dfa75944d6b104239
Zarmsen <zarmsen@hotmail.com>
2016-01-24 08:12:01 +0100

    create project Interpreter

---
+++ source/app/Patterns.Behavioral.Interpreter/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Patterns.Behavioral.Interpreter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+
+            Console.WriteLine(input);
+
+
+            Console.ReadKey();
+        }
+    }
+}
