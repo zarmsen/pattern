c270a9189ed4c0e6923f0bab6f63c9f11b4d559a
Zarmsen <zarmsen@hotmail.com>
2016-02-07 06:33:25 +0100

    create class Mediator

---
+++ source/app/Patterns.Behavioral.Mediator/Mediator.cs
@@ -0,0 +1,20 @@
+﻿using Patterns.Core.Game;
+
+namespace Patterns.Behavioral.Mediator
+{
+    internal abstract class Mediator
+    {
+        public abstract void Register(Colleague colleague);
+
+        public abstract void Send(string message, Colleague colleague);
+    }
+
+
+    internal abstract class Colleague : PlayerBase
+    {
+        private Colleague(string name, Position sartPosition)
+            : base(name, sartPosition)
+        {
+        }
+    }
+}
