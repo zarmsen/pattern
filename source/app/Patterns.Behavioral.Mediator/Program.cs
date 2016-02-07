df4d1379143a5c6a2ac8ff43e4c6b97ca5942dd9
Zarmsen <zarmsen@hotmail.com>
2016-02-07 06:21:58 +0100

    create project Mediator

---
+++ source/app/Patterns.Behavioral.Mediator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Patterns.Behavioral.Mediator
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.ReadKey();
+        }
+    }
+}
