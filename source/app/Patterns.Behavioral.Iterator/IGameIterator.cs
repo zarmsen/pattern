335f7c86e8e327c6bf5776fb0b1ae4a543669066
Zarmsen <zarmsen@hotmail.com>
2016-02-07 05:26:43 +0100

    create interface IGameIterator

---
+++ source/app/Patterns.Behavioral.Iterator/IGameIterator.cs
@@ -0,0 +1,13 @@
+﻿using Patterns.Core.Game;
+
+namespace Patterns.Behavioral.Iterator
+{
+    internal interface IGameIterator
+    {
+        PlayerBase CurrentItem { get; }
+
+        PlayerBase First();
+
+        PlayerBase Next();
+    }
+}
