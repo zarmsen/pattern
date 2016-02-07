13f88ae234bb7e7068fb37ed44aa60ee0cf87fe9
Zarmsen <zarmsen@hotmail.com>
2016-02-07 05:22:10 +0100

    create project Patterns.Behavioral.Iterator

---
+++ source/app/Patterns.Behavioral.Iterator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Patterns.Behavioral.Iterator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadKey();
+        }
+    }
+}
