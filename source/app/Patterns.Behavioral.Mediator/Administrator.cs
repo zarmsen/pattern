cde6b7e134c23050f135092ec1d018b35ab002fc
Zarmsen <zarmsen@hotmail.com>
2016-02-07 06:48:37 +0100

    create concrete collegues

---
+++ source/app/Patterns.Behavioral.Mediator/Administrator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Patterns.Behavioral.Mediator
+{
+    internal class Administrator : Colleague
+    {
+        public Administrator(string name)
+            : base($"Admin:{name}") {}
+
+        public override void Receive(Colleague @from, string message)
+        {
+            Console.WriteLine("Admin {0} says to {1}: '{2}'", @from, Name, message);
+        }
+    }
+}
---
+++ source/app/Patterns.Behavioral.Mediator/Participant.cs
@@ -0,0 +1,8 @@
+﻿namespace Patterns.Behavioral.Mediator
+{
+    internal class Participant : Colleague
+    {
+        public Participant(string name)
+            : base(name) {}
+    }
+}
