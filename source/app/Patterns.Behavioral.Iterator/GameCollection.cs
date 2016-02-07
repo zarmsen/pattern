2fd8f0f242e2c79a13ba1c72dd27bee1bb326fd4
Zarmsen <zarmsen@hotmail.com>
2016-02-07 05:39:52 +0100

    implement collection

--- source/app/Patterns.Behavioral.Iterator/GameCollection.cs
+++ source/app/Patterns.Behavioral.Iterator/GameCollection.cs
@@ -1,9 +1,18 @@
-﻿namespace Patterns.Behavioral.Iterator
+﻿using System.Collections;
+
+namespace Patterns.Behavioral.Iterator
 {
-    internal class GameCollection : IGameCollection {
+    internal class GameCollection : IGameCollection
+    {
+        private readonly ArrayList _items = new ArrayList();
+
+        public int Count => _items.Count;
+
+        public object this[int index] { get { return _items[index]; } set { _items.Add(index); } }
+
         public IGameIterator CreateIterator()
         {
-            return null;
+            return new GameIterator(this);
         }
     }
 }
