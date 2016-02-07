dab2067907adf4558dc2855bb04d217409a72f6e
Zarmsen <zarmsen@hotmail.com>
2016-02-07 05:31:05 +0100

    create class GameIterator

---
+++ source/app/Patterns.Behavioral.Iterator/GameIterator.cs
@@ -0,0 +1,19 @@
+﻿using Patterns.Core.Game;
+
+namespace Patterns.Behavioral.Iterator
+{
+    internal class GameIterator : IGameIterator
+    {
+        public PlayerBase CurrentItem { get; }
+
+        public PlayerBase First()
+        {
+            return null;
+        }
+
+        public PlayerBase Next()
+        {
+            return null;
+        }
+    }
+}
